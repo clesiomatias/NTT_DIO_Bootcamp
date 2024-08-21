766fb4421dd57096e48b9563005b9037310c0647
clesiomatias <clesiofmatias@gmail.com>
2024-08-21 17:18:47 -0300

    initial

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages     
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as f:
     page_description = f.read()
-    
+
 with open("requirements.txt", "r") as f:
     requirements = f.read().splitlines()
-    
+
 setup(
     name="image-processing-package",
     version="0.0.1",
@@ -13,8 +13,8 @@ setup(
     description="A package for image processing",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    url="https://github.com/clesiomatias/NTT_DIO_Bootcamp/",
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
